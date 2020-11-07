0dde78f5380d6ff0bf4af46c7c5f6a33ba2c8262
volcan96 <20894534+volcan96@users.noreply.github.com>
2020-11-07 15:00:14 -0800

    Update create-compose-files.sh

--- create-compose-files.sh
+++ create-compose-files.sh
@@ -8,6 +8,6 @@ current_time=$(date "+%Y.%m.%d-%H.%M.%S")
 docker ps --format '{{.Names}}' > containers.txt
 while IFS="" read -r p || [ -n "$p" ]
 do
-  docker run --rm -v /var/run/docker.sock:/var/run/docker.sock red5d/docker-autocompose $p > "$backup_dir/$p-$current_time.yaml"
+  docker run --rm -v /var/run/docker.sock:/var/run/docker.sock volcan96/docker-autocompose $p > "$backup_dir/$p-$current_time.yaml"
 done < containers.txt
 find "$backup_dir" -name "*.yaml" -mtime +5 -exec rm {} \;
