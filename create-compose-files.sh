41be4056c5ebc0262f711dc75a69cdae2076de36
volcan96 <20894534+volcan96@users.noreply.github.com>
2020-11-07 14:57:28 -0800

    Create create-compose-files.sh

---
+++ create-compose-files.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+# Path to backup location
+backup_dir="$HOME/compose-backups"
+
+docker pull volcan96/docker-autocompose
+current_time=$(date "+%Y.%m.%d-%H.%M.%S")
+docker ps --format '{{.Names}}' > containers.txt
+while IFS="" read -r p || [ -n "$p" ]
+do
+  docker run --rm -v /var/run/docker.sock:/var/run/docker.sock red5d/docker-autocompose $p > "$backup_dir/$p-$current_time.yaml"
+done < containers.txt
+find "$backup_dir" -name "*.yaml" -mtime +5 -exec rm {} \;
